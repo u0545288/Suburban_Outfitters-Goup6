53322ba2013b7b1e28b5675df9b92247d1503a91
Siddharth-H <siddharthahatkar@gmail.com>
2020-03-17 16:47:46 -0600

    Problem in 4200

--- suburban-outfitters/src/app/app.module.ts
+++ suburban-outfitters/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +28,8 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
