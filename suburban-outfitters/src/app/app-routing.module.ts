2786750104784d82f1e59c27ea9cc59cc556b8ca
jspenc72 <jspenc72@gmail.com>
2020-03-17 16:53:04 -0600

    Update app-routing.module.ts

--- suburban-outfitters/src/app/app-routing.module.ts
+++ suburban-outfitters/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'; 
+import { AccountComponent } from './account/account.component'; 
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'account', component: AccountComponent },
 
-const routes: Routes = [];
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
